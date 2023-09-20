89ee8466704d26c9f23d9eba28193168e2b6b030
vishwa <saivishwa.gaddam.com>
2023-09-20 11:54:41 -0700

    git push origin

---
+++ lambda.py
@@ -0,0 +1,23 @@
+def lambda_handler(event, context):
+            return "Hello World!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pandas as pd
+
+# def lambda_handler(event, context):
+#     d = {'col1': [1,2], 'col2': [3,6]}
+#     df = pd.DataFrame(data=d)
+#     print(df)
+#     print('Done 2nd version')
